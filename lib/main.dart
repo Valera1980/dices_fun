2403b62fd5bb818570ae6d432f6a6d23adcf2a08
Valera <gavrilow777@gmail.com>
2020-04-26 10:54:33 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  return runApp(new ScaffoldWrapper());
+}
+class ScaffoldWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
